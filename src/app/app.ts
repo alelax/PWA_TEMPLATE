14eeadd293c24adc63dcf97e496f3dd4466d871c
alelax <alessandro.lausdei@gmail.com>
2025-10-04 12:07:47 +0200

    add online/offline check

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NetworkService } from './core/network/network';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  template: `
+    @if (isNewVersionReady()) {
+      <span>🆕 New version available! Please reload the application.</span>
+      <button (click)="reload()">Reload</button>
+    }
+    <h1>Welcome to {{ title() }}!!</h1>
+    @if (network.isOffline()) {
+      <span>🚫 Offline</span>
+    }
+
+    <router-outlet />
+  `,
   styleUrl: './app.scss'
 })
 export class App {
+  protected readonly network = inject(NetworkService);
+  private readonly swUpdate = inject(SwUpdate);
+
   protected readonly title = signal('PWA_TEMPLATE');
+  protected readonly isNewVersionReady = signal(false);
+
+  constructor() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates
+        .pipe(filter(event => event.type === 'VERSION_READY'))
+        .subscribe((event: VersionReadyEvent) => {
+          this.isNewVersionReady.set(true);
+        });
+    }
+  }
+
+  protected reload() {
+    window.location.reload();
+  }
 }
